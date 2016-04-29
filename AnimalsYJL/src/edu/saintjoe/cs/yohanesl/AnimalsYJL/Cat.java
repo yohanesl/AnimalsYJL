146284f6b800505b596117702a156ec4b627e21e
Yohanes Lee <hniclee@gmail.com>
2016-04-29 06:35:34 -0400

    cat finished

--- AnimalsYJL/src/edu/saintjoe/cs/yohanesl/AnimalsYJL/Cat.java
+++ AnimalsYJL/src/edu/saintjoe/cs/yohanesl/AnimalsYJL/Cat.java
@@ -1,10 +1,5 @@
 package edu.saintjoe.cs.yohanesl.AnimalsYJL;
 
-public class Cat {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+public class Cat extends Animal {
 
 }
