8f75ac8e5d84e75622a9de43603c731b52a74fe5
Yohanes Lee <hniclee@gmail.com>
2016-04-29 06:57:03 -0400

    Animal class finished, fixed errors in tester

--- AnimalsYJL/src/edu/saintjoe/cs/yohanesl/AnimalsYJL/Animal.java
+++ AnimalsYJL/src/edu/saintjoe/cs/yohanesl/AnimalsYJL/Animal.java
@@ -1,10 +1,49 @@
 package edu.saintjoe.cs.yohanesl.AnimalsYJL;
 
 public class Animal {
+	
+		String picture;
+		String location;
+		int weight;
+		
+		public Animal() {
+			this.location = "Nowhere";
+			this.picture = "No picture";
+			
+		}
+		
+		public Animal(String startingLocation) {
+			this.location = startingLocation;
+			
+		}
+		
+		public Animal(int startingWeight) {
+			this.weight = startingWeight;
+		}
+		
+		
+		public Animal(String startLocation, String startPicture, int startWeight) {
+			this.weight = startWeight;
+			this.location = startLocation;
+			this.picture = startPicture;
+		}
+		
+		public Animal(int startWeight, String secondParam) {
+			
+		}
+		
+		public Animal(String firstParm, int startWeight) {
+			
+		}
+		
+		public String makeNoise() {
+			return ("Ommmmmmmmmmmmmm");
+		}
+		
+		public void eat () {
+			System.out.println("Animal is eating");
+		}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+	
 
 }
