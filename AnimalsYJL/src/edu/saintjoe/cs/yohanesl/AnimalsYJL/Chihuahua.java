c31aae34c0010810f0880cf03da0a6cbae947e7c
Yohanes Lee <hniclee@gmail.com>
2016-04-29 06:38:11 -0400

    Chihuahua finished

--- AnimalsYJL/src/edu/saintjoe/cs/yohanesl/AnimalsYJL/Chihuahua.java
+++ AnimalsYJL/src/edu/saintjoe/cs/yohanesl/AnimalsYJL/Chihuahua.java
@@ -1,10 +1,10 @@
 package edu.saintjoe.cs.yohanesl.AnimalsYJL;
 
-public class Chihuahua {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+public class Chihuahua extends Dog {
+	
+		public String makeNoise() {
+			return "Yip Yip Yip Yip";
+			
+		}
 
 }
