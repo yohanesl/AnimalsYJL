e832eb5dced19e420df4a2c44e5875033bffd6df
Yohanes Lee <hniclee@gmail.com>
2016-04-29 06:28:59 -0400

    Initial Commit

---
+++ AnimalsYJL/src/edu/saintjoe/cs/yohanesl/AnimalsYJL/ArrayPlay.java
@@ -0,0 +1,20 @@
+package edu.saintjoe.cs.yohanesl.AnimalsYJL;
+
+public class ArrayPlay {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] myArray;
+		myArray = new int[10];
+		
+		Animal[] animals;
+		
+		animals = new Animal[3];
+		
+		myArray[0] = 8;
+		myArray[1] = 9;
+		 System.out.println("I have stored " + myArray[1] + "at the 1");
+
+	}
+
+}
