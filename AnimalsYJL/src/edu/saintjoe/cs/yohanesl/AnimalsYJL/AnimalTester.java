a3563273c732fcb4e858bb6f0944074aff861b7f
Yohanes Lee <hniclee@gmail.com>
2016-04-29 06:48:36 -0400

    Animal tester with errors

--- AnimalsYJL/src/edu/saintjoe/cs/yohanesl/AnimalsYJL/AnimalTester.java
+++ AnimalsYJL/src/edu/saintjoe/cs/yohanesl/AnimalsYJL/AnimalTester.java
@@ -3,7 +3,13 @@ package edu.saintjoe.cs.yohanesl.AnimalsYJL;
 public class AnimalTester {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Demonstration of various constructors
+		Animal myPet = new Animal();
+		Animal myLocalPet = new Animal(23);
+		
+		System.out.println("My animal has " + myPet.location + " as its location");
+		System.out.println("My animal has " + myLocalPet.location + " as its location");
+		
 
 	}
 
