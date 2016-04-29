9553caafa39fc82218613e326709fbd6f6a3a6df
Yohanes Lee <hniclee@gmail.com>
2016-04-29 06:32:08 -0400

    All classes

---
+++ AnimalsYJL/src/edu/saintjoe/cs/yohanesl/AnimalsYJL/Chihuahua.java
@@ -0,0 +1,10 @@
+package edu.saintjoe.cs.yohanesl.AnimalsYJL;
+
+public class Chihuahua {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ AnimalsYJL/src/edu/saintjoe/cs/yohanesl/AnimalsYJL/Dog.java
@@ -0,0 +1,10 @@
+package edu.saintjoe.cs.yohanesl.AnimalsYJL;
+
+public class Dog {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
