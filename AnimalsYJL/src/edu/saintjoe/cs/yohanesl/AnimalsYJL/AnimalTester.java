7ff662fbfc7022ec5048f68d14dc5aeb66740d02
Yohanes Lee <hniclee@gmail.com>
2016-04-29 06:51:09 -0400

    animal tester comments

--- AnimalsYJL/src/edu/saintjoe/cs/yohanesl/AnimalsYJL/AnimalTester.java
+++ AnimalsYJL/src/edu/saintjoe/cs/yohanesl/AnimalsYJL/AnimalTester.java
@@ -4,8 +4,17 @@ public class AnimalTester {
 
 	public static void main(String[] args) {
 		// Demonstration of various constructors
+		// This instantiation calls the "default" (no parameter) constructor
 		Animal myPet = new Animal();
+		
+		// This one uses the one-parameter constructor
 		Animal myLocalPet = new Animal(23);
+		
+		//This instantiation uses the third constructor
+		// uses the fourth
+		// uses the fifth
+		// uses the sixth
+		
 		
 		System.out.println("My animal has " + myPet.location + " as its location");
 		System.out.println("My animal has " + myLocalPet.location + " as its location");
